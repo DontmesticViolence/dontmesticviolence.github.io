4cf1ae95eacbb8d0ae9bc8067281c637ac813923
Amar Persaud <tehspartaa@gmail.com>
2018-11-25 00:50:37 -0500

    Updated input styles and added search
    Made the input for the search less ugly and implemented the js to search

--- script.js
+++ script.js
@@ -26,4 +26,20 @@ function SafetyExit(){
 	console.log("Safety Exit activated");
 }
 
-
+function search(id){
+	el = document.getElementById(id);
+	//Open a new tab/window with the search.
+	loc = "https://www.domesticshelters.org/search#?q=" + encodeURIComponent(el.value);
+	console.log(location);
+	w = window.open(loc);
+}
+function checksearch(e){
+    e = e || window.event;
+	switch(e.which || e.keyCode) {
+		case 13:
+			search("shelter-search");
+			break;
+		default:
+			return;
+	}
+}
